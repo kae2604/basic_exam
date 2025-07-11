472c156a76285389f8a4503dbe3b3675b1a8d845
kae2604 <kae2604@ukr.net>
2025-07-11 23:34:39 +0300

    first test commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 3000,
+    loop: true,
+    center: true,
+    startPosition: 1,
+    responsive:{
+        0:{
+            items:1
+        },
+        1100:{
+            items:3,
+
+        }
+    }
+
+})
+
+
+
